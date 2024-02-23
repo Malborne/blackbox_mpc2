edb4b4b2b3f5ee9e207094bb1253c205e0f0ec11
Nicolae Lucian Mihet <nlm@hiof.no>
2024-02-23 16:37:41 +0100

    removing the tensorflow version requirement

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 import setuptools
 setuptools.setup(
      name='blackbox_mpc',
-     version='0.3',
+     version='0.3.0.1',
      author="Ossama Ahmed, Jonas Rothfuss",
      author_email="ossama.ahmed@mail.mcgill.ca, jonas.rothfuss@inf.ethz.ch",
      description="BlackBox MPC - Model Predictive Control with"
                   "sampling based optimizers",
-     url="https://github.com/ossamaAhmed/blackbox_mpc",
+     url="https://github.com/Malborne/blackbox_mpc2.git",
      packages=setuptools.find_packages(),
      install_requires=[
-        'tensorflow==2.0.0',
-        'tensorflow-probability==0.8.0rc0',
+        'tensorflow',
+        'tensorflow-probability',
         'gym',
         'numpy',
         'sphinx',
